a6245aedaa8c9fec85ecaa3a174efa3f828218f3
WeLoveJavaScript <50992017+WeLoveJavaScript@users.noreply.github.com>
2020-10-12 18:01:04 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Discord Panel TutoHacks
+
+
+## Was macht diese Website besonders? 💎
+
+*   ⌛ **Einfache installation:** kopiere die ZIP, entpacke sie und öffnen sie dann [DiscordPanelTutoHacks](../index.html).
+*   🔒 **Sicherheit:** Dein Bot-Token ist lokal gespeichert, es besteht keine Gefahr, dass er gestohlen wird.
+*   👀 **Open source:** du hast Ideen die du selber umsetzen kannst? Benutze den Code und programmiere es rein!
+*   👀 **In entwicklung:** du hast Ideen die du nicht selber umsetzen kannst? Schreibe mir auf Discord oder komm einfach auf meinen [Discord](https://discord.gg/DsqvUFn)!
+
+## Installation
+
+1.  Downloade den letzen [release](https://github.com/welovejavascript/DiscordPanel/releases)
+2.  Entpacke es mit WinRAR, 7zip, WinZip...
+3.  Gehe in das [Discord Developer Portal](https://discord.com/developers/applications) und hole dir einen Bot token
+4.  Öffne die [index.html](../index.html)
+5.  Füge deinen Token ein, klicke <kbd>ok</kbd> order klick <kbd>enter</kbd>
+6.  FERTIG !
+
+
+## programmiert mit 🔧
+
+*   [discord.js](https://github.com/discordjs/discord.js/tree/webpack)
+*   [jQuery](https://jquery.com/)
+*   [Bootstrap](https://getbootstrap.com/)
+
+### Übersetzungen 🌐
+
+| Author                                                  | Language                 |
+| ------------------------------------------------------- | ------------------------ |
+| [TutoHacks](https://github.com/welovejavascript)        | Deutsch                  |
+
+### Anderes 👍
+
+| Author                                              |
+| ----------------------------------------------------|
+| [SincerelyFaust](https://github.com/SincerelyFaust) |
+
+## Copyrights and Licence
+
+Copyright © 2020-2021 [TutoHacks <tutohacks511@gmail.com>](https://github.com/welovejavascript)
+
+Dieses script ist mit [MIT](https://github.com/welovejavascript/DiscordPanel/license) lizensiert
+
+***
---
+++ locales.js
@@ -0,0 +1,69 @@
+const locales = {
+    "de": {
+        "cCode": "de",
+        "language": "Deutsch",
+        "text": {
+            "channelNameLabel": "Chat",
+            "scrollCheck": "Aktiviere auto scrollen",
+            "fileSent": "sende Dateien",
+            "attachmentTxt": "Attachments",
+            "privateMessages": "DM",
+            "userId": "User ID"
+        },
+        "buttons": {
+            "clearLastMessages": "Letzte Nachricht löschen",
+            "editToken": "Bot wechseln",
+            "refreshChat": "Chat neuladen",
+            "changeLanguage": "Change language",
+            "send": "Senden",
+            "leave": "Server verlassen",
+            "invite": "Einladung"
+        },
+        "infos": {
+            "owner": "Owner",
+            "members": "Spieler",
+            "vChannels": "Channel (voice)",
+            "tChannels": "Channel (text)",
+            "roles": "Rollen",
+            "channels": "Channel",
+            "emojis": "Emojis"
+        },
+        "headings": {
+            "lastMessages": "Letzte Nachricht",
+            "guildName": "Information",
+            "autoScroll": "Auto-Scroll",
+        },
+        "errors": {
+            "error": "ERROR",
+            "dm": "ERROR : DM",
+            "emptyMsg": "ERROR : LEERE MESSAGE",
+            "missingPermissions": "ERROR : FEHLENDE PERMISSIONS"
+        },
+        "token": {
+            "prompt": "Bitte gebe den Bot token an über den du schreiben willst!",
+            "confirmation": "Bist du bereit ?",
+            "invalid": "Kein Token angegeben oder ungültiger Token angegeben"
+        },
+        "messageType": {
+            "serverJoin": "joint einen Server",
+            "pin": "pinnt eine Nachricht an",
+            "channelNews": "fügte einen Remote-Kanal Nachrichten hinzu",
+            "boost": "boostet den Server"
+        },
+        "fileType": {
+            "unknown": "unknown",
+            "img": "image",
+            "doc": "document",
+            "pdf": "pdf",
+            "video": "video",
+            "audio": "audio"
+        },
+        "formatting": {
+            "bold": "Bold",
+            "emphasis": "Emphasis",
+            "underline": "Underline",
+            "strike": "Strikethrough",
+            "clear": "Clear formatting"
+        }
+    }
+};
